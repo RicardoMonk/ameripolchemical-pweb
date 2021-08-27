0b79803fd25258bfa14ae60444f9962bc5b94c2a
Ricardo Monk <ricardodgcvisual@gmail.com>
2021-08-27 18:48:05 -0500

    Barra de nav al 70% + add files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+JQuery('document').ready(function($){
+
+    var menuBtn = $('.menu-icon'),
+        menu = $('ul');
+
+        menuBtn.click(function(){
+
+            menu.addClass('show');
+        });
+
+    
+});
