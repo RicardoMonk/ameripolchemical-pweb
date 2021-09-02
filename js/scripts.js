c7e4c8b5377bd7f90d809b782502fb7cd9ef8994
Ricardo Monk <ricardodgcvisual@gmail.com>
2021-09-02 02:32:02 -0500

    added Loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+//this code work to give it style to menu icon
+
 const menuIcon = document.querySelector(".menu-icon");
 const menuOptions = document.querySelector(".menu-options");
 
 menuIcon.addEventListener("click", () => {
     menuOptions.classList.toggle("nav-menu_visible");
+});
+
+
+//this code work to animate loader
+
+window.addEventListener("load", function() {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
 });
