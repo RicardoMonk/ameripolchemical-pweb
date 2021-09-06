1af463e862a7926c9e0b0b047b28e60fe1a6a0a8
Ricardo Monk <ricardodgcvisual@gmail.com>
2021-09-05 23:48:48 -0500

    actual cambio de barra de menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,9 @@
 //this code work to give it style to menu icon
 
-const menuIcon = document.querySelector(".menu-icon");
-const menuOptions = document.querySelector(".menu-options");
+var MenuIcon = document.getElementById('menu-icon');
+var show = document.getElementById('menu');
 
-menuIcon.addEventListener("click", () => {
-    menuOptions.classList.toggle("nav-menu_visible");
-});
-
-
-//this code work to animate loader
-
-window.addEventListener("load", function() {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden";
-});
+MenuIcon.addEventListener('click', function(){
+    this.classList.toggle("close");
+    show.classList.toggle("show");
+})
