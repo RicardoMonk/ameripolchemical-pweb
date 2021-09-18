e8f291be7504d04cd5debf172bf7a5a50bf7a940
Ricardo Monk <ricardodgcvisual@gmail.com>
2021-09-18 13:15:45 -0500

    slider 95%

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,40 @@ window.addEventListener("load", function() {
     const loader = document.querySelector(".loader");
     loader.className += " hidden";
 });
+
+//this code work to carousel
+
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__lista'),{
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.carousel__indicadores',
+    arrows: {
+      prev: '.carousel__anterior',
+      next: '.carousel__siguiente'
+    },
+    responsive: [
+        {
+          // screens greater than >= 775px
+          breakpoint: 768,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            draggable: true,
+            slidesToShow: '2',
+            slidesToScroll: '2',
+
+          }
+        },{
+          // screens greater than >= 1024px
+          breakpoint: 1024,
+          settings: {
+            draggable: true,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+
+          }
+        }
+      ]
+    });
+});
